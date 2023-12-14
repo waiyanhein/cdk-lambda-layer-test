8ad2e945fcbe24a5018562f8195c93a0a33f8660
waiyanhein <iljimae.ic@gmail.com>
2023-12-14 09:46:28 +0000

    test

---
+++ dist/package/calculator.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+const add = (num1, num2) => {
+    return num1 + num2;
+};
+exports.add = add;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2FsY3VsYXRvci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9jYWxjdWxhdG9yLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUNPLE1BQU0sR0FBRyxHQUFHLENBQUMsSUFBWSxFQUFFLElBQVksRUFBRSxFQUFFO0lBQzlDLE9BQU8sSUFBSSxHQUFHLElBQUksQ0FBQztBQUN2QixDQUFDLENBQUE7QUFGWSxRQUFBLEdBQUcsT0FFZiIsInNvdXJjZXNDb250ZW50IjpbIlxuZXhwb3J0IGNvbnN0IGFkZCA9IChudW0xOiBudW1iZXIsIG51bTI6IG51bWJlcikgPT4ge1xuICAgIHJldHVybiBudW0xICsgbnVtMjtcbn1cbiJdfQ==
